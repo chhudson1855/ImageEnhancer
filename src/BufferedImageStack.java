9fb914fe086dd67205bf7325e1dc6c27f3a30679
ImportRandom0 <chhudson1855@gmail.com>
2024-10-17 14:42:54 -0500

    Created new file headers
    headers

--- src/BufferedImageStack.java
+++ src/BufferedImageStack.java
@@ -1,9 +1,47 @@
+import java.awt.image.*;
 import java.util.*;
 
 public class BufferedImageStack {
 	
-	public BufferedImageStack()
+	int[] FakeStack;
+	
+	public void push(BufferedImage someBufferedImage)
 	{
 		
+	}
+	
+	public boolean isEmpty()
+	{
+		return false;
+	}
+	
+	public BufferedImage get()
+	{
+		return null;
+	}
+	
+	public int getSize()
+	{
+		return 0;
+	}
+	
+	public int getArraySize()
+	{
+		return 0;
+	}
+	
+	public BufferedImage pull()
+	{
+		return null;
+	}
+	
+	public void pop()
+	{
+		
+	}
+	
+	public BufferedImageStack()
+	{
+		FakeStack = new int[2];
 	}
 }
